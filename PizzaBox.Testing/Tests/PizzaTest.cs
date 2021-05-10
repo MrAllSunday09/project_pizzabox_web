338c548c0980a712ad490d6b992a68f2f4580273
daniel henderson <drayhender@gmail.com>
2021-05-10 12:46:29 -0400

    implementing sonar cloud

---
+++ PizzaBox.Testing/Tests/PizzaTest.cs
@@ -0,0 +1,18 @@
+
+
+using PizzaBox.Domain.Models;
+using Xunit;
+
+namespace PizzaBox.Testing.Tests
+{
+  public class PizzaTests
+  {
+    [Fact]
+    public void Test_PizzaConstructor()
+    {
+      var sut = new Pizza();
+
+      Assert.NotNull(sut);
+    }
+  }
+}
